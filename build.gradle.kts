d879c10b62d06a471ca87e87c0093f34f76f48da
Joshua Gleitze <dev@joshuagleitze.de>
2026-03-04 22:17:27 +0100

    build: format gradle-wrapper.properties after updating it

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
   id("format")
 }
+
+tasks.wrapper {
+  distributionType = Wrapper.DistributionType.ALL
+
+  finalizedBy(tasks.spotlessPropertiesApply)
+}
+
+tasks.spotlessProperties {
+  mustRunAfter(tasks.wrapper)
+}
