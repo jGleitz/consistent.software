b93f7bf8a5ca1d265a7bda1f9394fd75bbf61521
Joshua Gleitze <dev@joshuagleitze.de>
2026-07-19 00:44:05 +0200

    build: fix :parsers:json build

--- parsers/json/build.gradle.kts
+++ parsers/json/build.gradle.kts
@@ -1,14 +1,23 @@
-@file:OptIn(ExperimentalKotlinGradlePluginApi::class)
+@file:OptIn(ExperimentalKotlinGradlePluginApi::class, ExperimentalBuildToolsApi::class)
 
+import org.jetbrains.kotlin.buildtools.api.ExperimentalBuildToolsApi
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode.Disabled
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.tasks.CInteropProcess
+import org.jetbrains.kotlin.konan.util.DependencyDirectories.localKonanDir
 
 plugins {
   id("format")
   id("multiplatform")
   id("nodejs")
-  id("io.github.tree-sitter.ktreesitter-plugin") version "0.25.0"
+  id("io.github.lemcoder.konanplugin") version "1.1.0"
+  id("io.github.tree-sitter.ktreesitter-plugin") version "0.25.1"
+}
+
+dependencies {
+  jvmMainApi(libs.ktreesitter.runtime)
+  nativeMainApi(libs.ktreesitter.runtime)
 }
 
 grammar {
@@ -24,6 +33,49 @@ grammar {
     )
 }
 
+val generateGrammarHeader =
+  tasks.register("generateGrammarHeader") {
+    inputs.property("grammarName", grammar.grammarName)
+    outputs.file(
+      layout.buildDirectory.dir("generated/src/c").zip(grammar.grammarName) { headerDir, grammarName ->
+        headerDir.file("tree-sitter-$grammarName.h")
+      },
+    )
+    mustRunAfter(tasks.generateGrammarFiles)
+
+    doLast {
+      val grammarName = inputs.properties["grammarName"] as String
+      outputs.files.singleFile.writeText(
+        """
+        |#ifndef TREE_SITTER_JSON_H_
+        |#define TREE_SITTER_JSON_H_
+        |typedef struct TSLanguage TSLanguage;
+        |#ifdef __cplusplus
+        |extern "C" {
+        |#endif
+        |const TSLanguage *tree_sitter_$grammarName(void);
+        |#ifdef __cplusplus
+        |}
+        |#endif
+        |#endif // TREE_SITTER_JSON_H_
+        |
+        """.trimMargin(),
+      )
+    }
+  }
+
+konanConfig {
+  targets.addAll(kotlin.targets.withType<KotlinNativeTarget>().map { it.konanTarget.name })
+  sourceDir = grammar.baseDir.map { it.resolve("src").path }
+  headerDir = generateGrammarHeader.map { it.outputs.files.singleFile.parent }
+  libName = grammar.grammarName.map { "tree-sitter-$it" }
+  outputDir = layout.buildDirectory.dir("konan-lib").map { it.asFile.path }
+  konanPath =
+    kotlin.compilerVersion.map { kotlinVersion ->
+      localKonanDir.listFiles()?.first { it.name.contains(kotlinVersion) }?.path
+    }
+}
+
 kotlin {
   for (sourceSetName in listOf("commonMain", "jvmMain", "nativeMain")) {
     sourceSets.named(sourceSetName) {
@@ -32,17 +84,25 @@ kotlin {
   }
 
   targets.withType<KotlinNativeTarget>().configureEach {
+    val target = this
     compilations.named("main").configure {
       cinterops {
         create(grammar.interopName.get()) {
-          packageName = grammar.packageName.get()
+          definitionFile = tasks.generateGrammarFiles.flatMap { it.generatedSrc.file("nativeInterop/${it.interopName}.def") }
+          includeDirs.allHeaders(generateGrammarHeader.map { it.outputs.files.singleFile.parent })
+          extraOpts("-libraryPath", konanConfig.outputDir.get() + "/" + target.konanTarget.name)
         }
       }
     }
   }
   compilerOptions {
     explicitApi = Disabled
+    freeCompilerArgs.add("-Xexpect-actual-classes")
   }
+}
+
+tasks.withType<CInteropProcess>().configureEach {
+  dependsOn(generateGrammarHeader, tasks.runKonanClang)
 }
 
 val pnpmInstall = nodeJs.registerPnpmInstall()
---
+++ parsers/json/src/jsMain/kotlin/TreeSitterJson.kt
@@ -0,0 +1,5 @@
+package io.github.treesitter.language.json
+
+actual object TreeSitterJson {
+  actual fun language(): Any = TODO("solution for JavaScript")
+}
